23b2f2d2810bf455d60a1fa856a479dac2cd7ce0
JFermepin <jfermepingaldo@frba.utn.edu.ar>
2025-08-27 17:18:38 +0000

    Agregados comandos para la consola (help y exit)

--- repl.go
+++ repl.go
@@ -1,8 +1,36 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+		commandName := input[0]
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
